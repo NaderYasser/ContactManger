9ff4d63e0ab30a0d45143f87139b1c20e36b9992
Nader Yasser <naderdd7@gmail.com>
2019-05-28 16:44:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById(‘root’));
 registerServiceWorker();
